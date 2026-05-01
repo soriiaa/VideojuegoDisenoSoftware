7701435f84f734fbf6b753294a5729ed996ec9a8
soriiaa <alex.soriaalcaraz@gmail.com>
2026-05-01 14:22:37 +0200

    Commit 4 - Creo la clase Entidad y sus atributos e inicializo los métodos.

--- src/main/java/com/videogame/videojuegodissotfware/model/entities/Entidad.java
+++ src/main/java/com/videogame/videojuegodissotfware/model/entities/Entidad.java
@@ -1,4 +1,23 @@
 package com.videogame.videojuegodissotfware.model.entities;
 
-public class Entidad {
+public abstract class Entidad {
+    private String nombre;
+    private int puntosVida;
+    private String estado;
+    private int dano;
+    private int resistencia;
+    private String textura; // Aquí tenemos que guardar la ruta al png, no el png como tal.
+    private int[][] posicion = new int[480][768];
+
+    public void moverse() {
+
+    }
+
+    public void atacar() {
+
+    }
+
+    public void fortalecer() {
+
+    }
 }
