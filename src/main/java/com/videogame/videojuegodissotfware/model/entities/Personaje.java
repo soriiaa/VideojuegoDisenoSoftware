3aab059a6727e610ddbc089550a019b0080203b9
soriiaa <alex.soriaalcaraz@gmail.com>
2026-05-01 14:32:57 +0200

    Commit 6 - Creo la clase Personaje, inicializo atributos y métodos.

--- src/main/java/com/videogame/videojuegodissotfware/model/entities/Personaje.java
+++ src/main/java/com/videogame/videojuegodissotfware/model/entities/Personaje.java
@@ -1,4 +1,25 @@
 package com.videogame.videojuegodissotfware.model.entities;
 
+import com.videogame.videojuegodissotfware.model.items.Item;
+
+import java.util.ArrayList;
+
 public class Personaje {
+    private int puntosVidaMax;
+    private int nivel;
+    private int oro;
+    private ArrayList<Item> listaItems;
+    private int sexo; // 0 hombre, 1 mujer
+
+    public void usarPocion() {
+
+    }
+
+    public void comprarItem() {
+
+    }
+
+    public void iniciarCombate() {
+
+    }
 }
