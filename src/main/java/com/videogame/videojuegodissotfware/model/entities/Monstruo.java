b77523c0873f0241265b77c59b34fb82fbdf5a1e
soriiaa <alex.soriaalcaraz@gmail.com>
2026-05-01 14:26:59 +0200

    Commit 5 - Creo la clase Monstruo y sus atributos.

--- src/main/java/com/videogame/videojuegodissotfware/model/entities/Monstruo.java
+++ src/main/java/com/videogame/videojuegodissotfware/model/entities/Monstruo.java
@@ -1,4 +1,9 @@
 package com.videogame.videojuegodissotfware.model.entities;
 
+import com.videogame.videojuegodissotfware.model.strategies.EstrategiaCombate;
+
 public class Monstruo {
+    private String categoria;
+    private EstrategiaCombate estrategiaCombate;
+    private int botin;
 }
