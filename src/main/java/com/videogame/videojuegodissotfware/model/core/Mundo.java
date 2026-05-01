dc647052ed42c10d482fcad1bebbbd26ed23fb3f
soriiaa <alex.soriaalcaraz@gmail.com>
2026-05-01 14:02:13 +0200

    Commit 2 - Creada la clase mundo y sus métodos inicializados

---
+++ src/main/java/com/videogame/videojuegodissotfware/model/core/Mundo.java
@@ -0,0 +1,29 @@
+package com.videogame.videojuegodissotfware.model.core;
+
+import com.videogame.videojuegodissotfware.model.entities.Monstruo;
+import com.videogame.videojuegodissotfware.model.entities.Personaje;
+
+import java.util.ArrayList;
+
+public class Mundo {
+    private ArrayList<Monstruo> listaMonstruos;
+    private Personaje personaje;
+    private long tiempoPartida;
+    private int[][] mapaBits = new int[15][24];
+
+    public void reiniciarMundo() {
+
+    }
+
+    public void pausar() {
+
+    }
+
+    public void comprobarResultado() {
+
+    }
+
+    public void finalizarPartida() {
+
+    }
+}
