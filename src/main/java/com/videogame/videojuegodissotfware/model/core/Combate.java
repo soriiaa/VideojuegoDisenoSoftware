0589d7c656900e7774a027dd6e2320e4a3c07932
soriiaa <alex.soriaalcaraz@gmail.com>
2026-05-01 14:08:28 +0200

    Commit 3 - Creada la clase combate e inicializados los getters y setters y atributos.

--- src/main/java/com/videogame/videojuegodissotfware/model/core/Combate.java
+++ src/main/java/com/videogame/videojuegodissotfware/model/core/Combate.java
@@ -1,4 +1,27 @@
 package com.videogame.videojuegodissotfware.model.core;
 
+import com.videogame.videojuegodissotfware.model.entities.Entidad;
+import com.videogame.videojuegodissotfware.model.entities.Monstruo;
+import com.videogame.videojuegodissotfware.model.entities.Personaje;
+
 public class Combate {
+    private Monstruo monstruo;
+    private Personaje personaje;
+    private Entidad ganador;
+
+    public void setMonstruo(Monstruo monstruo) {
+        this.monstruo = monstruo;
+    }
+
+    public void setPersonaje(Personaje personaje) {
+        this.personaje = personaje;
+    }
+
+    public void finalizarCombate() {
+
+    }
+
+    public void comprobarResultado() {
+
+    }
 }
