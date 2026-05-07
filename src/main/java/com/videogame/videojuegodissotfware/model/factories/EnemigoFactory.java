87ef419d44c9d193fe4806d27aa63f2ac92b61c0
nachoya19 <nacho.oya1@gmail.com>
2026-05-07 23:40:24 +0200

    patron factory abstract

--- src/main/java/com/videogame/videojuegodissotfware/model/factories/EnemigoFactory.java
+++ src/main/java/com/videogame/videojuegodissotfware/model/factories/EnemigoFactory.java
@@ -6,28 +6,10 @@ import com.videogame.videojuegodissotfware.model.entities.monstruos.Esqueleto;
 import com.videogame.videojuegodissotfware.model.entities.monstruos.Mago;
 import com.videogame.videojuegodissotfware.model.entities.monstruos.Orco;
 
-//SimpleFactory
-public class EnemigoFactory {
+public interface EnemigoFactory {
+    Monstruo crearEsqueleto();
+    Monstruo crearMago();
+    Monstruo crearOrco();
 
-    // Método estático para no tener que instanciar la fábrica
-    public static Monstruo crearEnemigo(String tipo) {
-
-        if (tipo.equalsIgnoreCase("Mago")) {
-            return new Mago();
-
-        } else if (tipo.equalsIgnoreCase("Orco")) {
-            return new Orco();
-
-        }
-        else if (tipo.equalsIgnoreCase("Esqueleto")) {
-            return new Esqueleto();
-
-        }
-        else if (tipo.equalsIgnoreCase("Dragon")) {
-            return new Dragon();
-
-        }
-
-        throw new IllegalArgumentException("El monstruo tipo " + tipo + " no existe en la fábrica.");
-    }
+    Monstruo crearDragon();
 }
