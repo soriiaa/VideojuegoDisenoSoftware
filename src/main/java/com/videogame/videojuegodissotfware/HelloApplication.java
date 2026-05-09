3503cf89cc536d12bfd2d0a0a5871b16528be1b8
soriiaa <alex.soriaalcaraz@gmail.com>
2026-05-09 13:38:56 +0200

    Commit 5 - Cambio el icono de la aplicacion y el nombre

--- src/main/java/com/videogame/videojuegodissotfware/HelloApplication.java
+++ src/main/java/com/videogame/videojuegodissotfware/HelloApplication.java
@@ -3,6 +3,7 @@ package com.videogame.videojuegodissotfware;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,10 +14,11 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/launch-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setScene(scene);
-        stage.setTitle("Nombre del videogame");
+        stage.setTitle("Mazmorras");
         stage.setWidth(1280);
         stage.setHeight(720);
         stage.setResizable(false);
+        stage.getIcons().add(new Image(HelloApplication.class.getResourceAsStream("/com/videogame/videojuegodissotfware/images/icono.png")));
         stage.show();
     }
 
